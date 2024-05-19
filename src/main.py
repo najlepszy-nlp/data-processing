9f28db31dea3692d705af29be8dbdb279d9fdcf1
matined <nedzimateusz@gmail.com>
2024-05-19 17:03:14 +0200

    fix: added locacation to api response

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 import logging
+import jsonlines
 from fastapi import FastAPI
 from gliner import GLiNER
 import pandas as pd
 from transformers import pipeline
 
-from utils.gliner import process_row
+from utils.processing import process_row
 
 INPUT_DATA_PATH = "/mnt/data/data.csv"
+INPUT_CITIES_PATH = "/mnt/data/cities.csv"
+OUTPUT_DATA_PATH = "/mnt/data/data_processed.jsonl"
 
 logging.basicConfig(level=logging.INFO)
 
 _data: list[dict] = []
-
-ner = GLiNER.from_pretrained("urchade/gliner_medium-v2.1")
-qa_model_name = "deepset/roberta-base-squad2"
-qa = pipeline("question-answering", model=qa_model_name, tokenizer=qa_model_name)
 
 app = FastAPI()
 
@@ -26,23 +25,39 @@ async def root():
 
 @app.get("/data")
 async def data(skip: int = 0, limit: int = 10):
+    global _data
+    if not _data:
+        with jsonlines.open(OUTPUT_DATA_PATH) as reader:
+            _data = list(reader.iter())
+
     return _data[skip : skip + limit]
 
 
 @app.post("/processing")
 async def processing():
+    logging.info("Loading models...")
+    ner = GLiNER.from_pretrained("urchade/gliner_medium-v2.1")
+    qa_model_name = "deepset/roberta-base-squad2"
+    qa = pipeline("question-answering", model=qa_model_name, tokenizer=qa_model_name)
+    logging.info("Models loaded.")
+
     logging.info("Processing data...")
 
-    df = pd.read_csv(INPUT_DATA_PATH, delimiter=";")
+    data_df = pd.read_csv(INPUT_DATA_PATH, delimiter=";")
+    cities_df = pd.read_csv(INPUT_CITIES_PATH, delimiter=",")
 
     global _data
     _data = []
 
-    for i, row in df.head(n=30).iterrows():
-        logging.info(f"Processing row {i + 1}/{len(df)}")
-        _data.append(process_row(row, ner, qa))
+    for i, row in data_df.head(n=30).iterrows():
+        logging.info(f"Processing row {i + 1}/{len(data_df)}")
+        _data.append(process_row(row, ner, qa, cities_df))
+
+    with jsonlines.open(OUTPUT_DATA_PATH, "w") as writer:
+        writer.write_all(_data)
 
     logging.info("Data processed.")
+
     return {"status": "done"}
 
 
---
+++ src/utils/processing.py
@@ -0,0 +1,126 @@
+import datetime
+from dateutil.parser import parse
+
+import pandas as pd
+from transformers import Pipeline
+from gliner import GLiNER
+
+
+def ask_questions(qa: Pipeline, context: str):
+    results = [None] * 5
+
+    # Initial question
+    question_what_happened = {"question": "What happened?", "context": context}
+    answer_what_happened = qa(question_what_happened)
+    results[2] = answer_what_happened["answer"]
+
+    # Questions about what happened before
+    question_before_1 = {
+        "question": f'What happened before {answer_what_happened["answer"]}?',
+        "context": context,
+    }
+    answer_before_1 = qa(question_before_1)
+    results[1] = answer_before_1["answer"]
+
+    question_before_2 = {
+        "question": f'What happened before {answer_before_1["answer"]}?',
+        "context": context,
+    }
+    answer_before_2 = qa(question_before_2)
+    results[0] = answer_before_2["answer"]
+
+    # Questions about what happened after
+    question_after_1 = {
+        "question": f'What happened after {answer_what_happened["answer"]}?',
+        "context": context,
+    }
+    answer_after_1 = qa(question_after_1)
+    results[3] = answer_after_1["answer"]
+
+    question_after_2 = {
+        "question": f'What happened after {answer_after_1["answer"]}?',
+        "context": context,
+    }
+    answer_after_2 = qa(question_after_2)
+    results[4] = answer_after_2["answer"]
+
+    return results
+
+
+def process_row(row: pd.Series, ner: GLiNER, qa: Pipeline, cities_df: pd.DataFrame):
+    LABELS = [
+        "day when accident happened",
+        "time when accident happened",
+        "vehicle",
+        "casualties",
+        "age of people who died",
+    ]
+
+    publish: str = row["Publish"]
+    place: str = row["Place"]
+    text: str = row["RAW_TEXT"]
+
+    results = {
+        "place": {
+            "name": place.lower(),
+            "lat": None,
+            "lng": None,
+        },
+        "date": None,
+        "time": None,
+        "vehicles": [],
+        "casualties": 0,
+        "ageOfCasualties": [],
+        "injuries": 0,
+        "reason": "",
+        "sequenceOfEvents": [],
+    }
+
+    cities_df["city"] = cities_df["city"].str.lower()
+    if place.lower() in cities_df["city"].values:
+        city = cities_df[cities_df["city"] == place.lower()]
+        results["place"]["lat"] = city["lat"].values[0]
+        results["place"]["lng"] = city["lng"].values[0]
+
+    entities = ner.predict_entities(text, LABELS)
+
+    seen_casualities = []
+
+    for entity in entities:
+        label, value = entity["label"], entity["text"]
+
+        if label == "day when accident happened" and not results["date"]:
+            try:
+                publish_weekday = parse(publish).weekday()
+                accident_weekday = parse(value.split(" ")[0]).weekday()
+                results["date"] = (
+                    parse(publish)
+                    - datetime.timedelta(days=(publish_weekday - accident_weekday))
+                ).strftime("%Y-%m-%d")
+            except:
+                pass
+
+        elif label == "time when accident happened" and not results["time"]:
+            results["time"] = value
+
+        elif label == "vehicle" and value.lower() not in results["vehicles"]:
+            results["vehicles"].append(value.lower())
+
+        elif label == "casualties" and value.lower() not in seen_casualities:
+            results["casualties"] += 1
+            seen_casualities.append(value.lower())
+
+        elif label == "age of people who died":
+            results["ageOfCasualties"].append(value)
+
+    if results["casualties"] < len(results["ageOfCasualties"]):
+        results["casualties"] = len(results["ageOfCasualties"])
+
+    answer_reason = qa({"question": "How did the vehicle crash?", "context": text})
+    answer_injuries = qa({"question": "How many people were injured?", "context": text})
+
+    results["injuries"] = answer_injuries["answer"]
+    results["reason"] = answer_reason["answer"]
+    results["sequenceOfEvents"] = ask_questions(qa, text)
+
+    return results
